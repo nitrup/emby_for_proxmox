d1167bad5098ff90322ad0eb77f3ba79b0f472ad
Ced_Cour <35567280+nitrup@users.noreply.github.com>
2024-12-23 10:27:12 +0100

    Update emby-install.sh

--- emby-install.sh
+++ emby-install.sh
@@ -1,34 +1,25 @@
-APP="Emby"
-
-
 function update_script() {
     header_info
     check_container_storage
     check_container_resources
     if [[ ! -d /opt/emby-server ]]; then
-        msg_error "No ${APP} Installation Found!"
+        msg_error "No Emby Installation Found!"
         exit
     fi
     LATEST=$(curl -sL https://api.github.com/repos/MediaBrowser/Emby.Releases/releases | grep '"tag_name":' | cut -d'"' -f4)
-    msg_info "Stopping ${APP}"
+    msg_info "Stopping Emby"
     systemctl stop emby-server
-    msg_ok "Stopped ${APP}"
+    msg_ok "Stopped Emby"
 
-    msg_info "Updating ${APP}"
+    msg_info "Updating Emby"
     wget https://github.com/MediaBrowser/Emby.Releases/releases/download/${LATEST}/emby-server-deb_${LATEST}_amd64.deb &>/dev/null
     dpkg -i emby-server-deb_${LATEST}_amd64.deb &>/dev/null
     rm emby-server-deb_${LATEST}_amd64.deb
-    msg_ok "Updated ${APP}"
+    msg_ok "Updated Emby"
 
-    msg_info "Starting ${APP}"
+    msg_info "Starting Emby"
     systemctl start emby-server
-    msg_ok "Started ${APP}"
+    msg_ok "Started Emby"
     msg_ok "Updated Successfully"
     exit
 }
-
-start
-build_container
-description
-
-msg_ok "Completed Successfully!\n"
