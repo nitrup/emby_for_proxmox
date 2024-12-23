1ee9428c419212bc8be842d90188190cbdbecaad
Ced_Cour <35567280+nitrup@users.noreply.github.com>
2024-12-23 10:18:27 +0100

    Update emby-install.sh

--- emby-install.sh
+++ emby-install.sh
@@ -1,28 +1,6 @@
-#!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://emby.media/
-
 # App Default Values
 APP="Emby"
-var_tags="media"
-var_cpu="2"
-var_ram="2048"
-var_disk="8"
-var_os="ubuntu"
-var_version="22.04"
-var_unprivileged="1"
 
-# App Output & Base Settings
-header_info "$APP"
-base_settings
-
-# Core
-variables
-color
-catch_errors
 
 function update_script() {
     header_info
@@ -32,7 +10,7 @@ function update_script() {
         msg_error "No ${APP} Installation Found!"
         exit
     fi
-    LATEST=$(curl -sL https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/lastest | grep '"tag_name":' | cut -d'"' -f4)
+    LATEST=$(curl -sL https://api.github.com/repos/MediaBrowser/Emby.Releases/releases | grep '"tag_name":' | cut -d'"' -f4)
     msg_info "Stopping ${APP}"
     systemctl stop emby-server
     msg_ok "Stopped ${APP}"
@@ -55,6 +33,3 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8086${CL}"
