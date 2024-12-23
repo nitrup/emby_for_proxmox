30bb8dde688e2e9d91930bf78c830b1f7167de9f
Ced_Cour <35567280+nitrup@users.noreply.github.com>
2024-12-23 10:20:58 +0100

    Add files via upload

--- emby-install.sh
+++ emby-install.sh
@@ -1,6 +1,28 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://emby.media/
+
 # App Default Values
 APP="Emby"
+var_tags="media"
+var_cpu="2"
+var_ram="2048"
+var_disk="8"
+var_os="ubuntu"
+var_version="22.04"
+var_unprivileged="1"
 
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
 
 function update_script() {
     header_info
@@ -33,3 +55,6 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8086${CL}"
